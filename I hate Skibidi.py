f2b9106a1f77e94e3aaccf983fbf9262a3037ddf
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-04-17 15:24:22 -0500

    I hate Skibidi.py Has been enchanced

--- I hate Skibidi.py
+++ I hate Skibidi.py
@@ -1,15 +1,19 @@
-meme = input("What is the state that was turned into a dumb meme: ")
-while meme == ("Ohio"):
-    print("Congrats, you know dumb memes")
-    meme1 = input("What is the name of the popular meme icon which is also connected to trolling: ")
-    while meme1 == ("Troll Face"):
-        print("Lets keep going")
-        meme2 = input("What is the WORST MEME EVER!?!?!?: ")
-        while meme2 == ("Skibidi Toilet"):
-            print("Im sorry that you know of this nightmare")
+v = 2
+while(v == 2):
+    meme = input("What is the state that was turned into a dumb meme: ")
+    while meme == ("Ohio"):
+        print("Congrats, you know dumb memes")
+        meme1 = input("What is the name of the popular meme icon which is also connected to trolling: ")
+        while meme1 == ("Troll Face"):
+            print("Lets keep going")
+            meme2 = input("What is the WORST MEME EVER!?!?!?: ")
+            while meme2 == ("Skibidi Toilet"):
+                print("Im sorry that you know of this nightmare")
+                v *= 1234567890
+            else:
+                print("You don't know. Keep it that way")
+                v *= 1234567890
         else:
-            print("You don't know. Keep it that way")
+            print("Incorrect, Try again")
     else:
-        print("Incorrect, Try again")
-else:
-    print("Incorrect, Try again, (Use capitals)")
+        print("Incorrect, Try again, (Use capitals)")
